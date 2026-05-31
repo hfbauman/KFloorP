8dc7524249ff5f6b53d08e8ab718ad5a09ff90cd
Harrison Bauman <hfbauman@gmail.com>
2026-05-30 21:56:14 -0400

    Replace floorplan with simple table for controls

--- js/setup.js
+++ js/setup.js
@@ -4,7 +4,7 @@
 var timeoffset = 0;
 
 // your home floorplan file path. Ex: var floorplan1stFloor = "./images/floorplan.png"
-var floorplan1stFloor = "./images/floorplan.png"; // your floorplan image path for 1st floor
+var floorplan1stFloor = "./images/table.png"; // your floorplan image path for 1st floor
 var floorplan2ndFloor = "./images/floorplan.png"; // your floorplan image path for 2nd floor
 
 // below you need to add your DEVICES in the following format:
@@ -40,42 +40,15 @@ var floorplan2ndFloor = "./images/floorplan.png"; // your floorplan image path f
 
 // devices for 1st floor
 var devices1stFloor = [
-  ["binary_sensor.janela_escritorio", 640, 30, "window"],
-  ["binary_sensor.porta_entrada", 225, 510, "door"],
-  ["light.lamp_cozinha", 450, 510],
-  ["media_player.echo_dot_1", 115, 30, "alexa"],
-  ["media_player.echo_dot_5", 275, 380, "alexa"],
-  ["media_player.tv", 285, 285, "tv"],
-  ["media_player.tv_suite", 243, 83, "tv"],
-  ["switch.aquecedor_oleo_0", 545, 30, "heater"],
-  ["switch.aquecedor_0", 260, 30, "heater"],
-  ["switch.aquecedor_gas_0", 650, 530, "heater"],
-  ["switch.microondas_0", 530, 455, "microwave"],
-  ["switch.lava_loucas_0", 500, 530, "dishwasher"],
-  ["switch.lava_roupas_0", 593, 530, "washingmachine"],
-  ["switch.geladeira_0", 542, 530, "fridge"],
-  ["switch.filtro_0", 400, 530, "waterfountain"],
-  ["sensor.temp_externa", 605, 310, "temperature"],
-  ["sensor.temp_suite", 243, 146, "temperature"],
-  ["sensor.temp_sala_temperature", 275, 440, "temperature"],
-  ["weather.apartamentocta", 110, 210, "5dayforecast"],
-  ["weather.cta_weather", 190, 350]
+    //OFFICE
+    ["sensor.office_temperature_rounded", 150, 40, "temperature"],
+    ["sensor.office_humidity_rounded", 150, 120, "humidity"],
+
+    //LIVING ROOM
+    ["light.living_room_lights", 220, 275]
 ];
 
 // devices for the second floor
 // leave empty if you only have 1 floor
 // empty example: var devices2ndFloor = [];
-var devices2ndFloor = [
-  ["binary_sensor.janela_escritorio", 640, 30, "window"],
-  ["binary_sensor.janela_suite", 194, 20, "window"],
-  ["light.lamp_escr", 565, 83],
-  ["light.lamp_suite", 183, 96],
-  ["light.lamp_varanda", 595, 370],
-  ["media_player.echo_dot_1", 115, 30, "alexa"],
-  ["media_player.tv", 285, 285, "tv"],
-  ["switch.geladeira_0", 542, 530, "fridge"],
-  ["switch.filtro_0", 400, 530, "waterfountain"],
-  ["sensor.temp_externa", 625, 420, "temperature"],
-  ["sensor.temp_suite", 243, 146, "temperature"],
-  ["sensor.temp_sala_temperature", 275, 440, "temperature"]
-];
+var devices2ndFloor = [];
